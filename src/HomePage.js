594a7e55dd04d9c7dfdac3322e2e24e8ff99293b
github username <github email address>
2018-10-12 11:19:40 +0100

    added homepage component

---
+++ src/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+export default class HomePage extends React.Component {
+
+  state = {
+    stylist_listings: []
+    requests: []
+  }
+
+  render () {
+    return(
+
+      <div>testing</div>
+
+    )
+  }
+
+
+
+
+
+
+
+}
