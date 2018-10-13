147d84d819f3af8c4dceb206b86ffd53a98e25ca
github username <github email address>
2018-10-13 14:14:54 +0100

    added search to StylistContainer

--- src/containers/StylistsContainer.js
+++ src/containers/StylistsContainer.js
@@ -4,11 +4,33 @@ import Stylist from '../components/Stylist'
 import Search from '../components/Search'
 
 export default class StylistsContainer extends React.Component {
+
+  state = {
+    searchQuery: ""
+  }
+
+  updateSearch = (searchQuery) => this.setState({ searchQuery })
+
+  filterStylists = () =>
+    this.props.stylists.filter(stylist => {
+      const first_name = stylist.first_name.toLowerCase()
+      const last_name = stylist.last_name.toLowerCase()
+      const bio = stylist.bio.toLowerCase()
+      const area = stylist.area.toLowerCase()
+      const searchQuery = this.state.searchQuery.toLowerCase()
+
+      return first_name.includes(searchQuery)
+        || last_name.includes(searchQuery)
+        || bio.includes(searchQuery)
+        || area.includes(searchQuery)
+    })
+
+
   render () {
     return(
       <div>
-        <Search/>
-        {this.props.stylists.map(stylist =>
+        <Search updateSearch={this.updateSearch}/>
+        {this.filterStylists().map(stylist =>
           <Stylist stylist={stylist}/>
         )}
       </div>
