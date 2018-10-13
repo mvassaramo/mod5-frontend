3f707af357196e6b88d42fc5b8b65da7355d5dac
github username <github email address>
2018-10-13 14:12:44 +0100

    added search functionality to Muas Container

--- src/containers/MuasContainer.js
+++ src/containers/MuasContainer.js
@@ -4,11 +4,34 @@ import Stylist from '../components/Stylist'
 import Search from '../components/Search'
 
 export default class MuasContainer extends React.Component {
+
+  state = {
+    searchQuery: ""
+  }
+
+  updateSearch = (searchQuery) => this.setState({ searchQuery })
+
+  filterMuas = () =>
+    this.props.stylists.filter(stylist => {
+      const first_name = stylist.first_name.toLowerCase()
+      const last_name = stylist.last_name.toLowerCase()
+      const bio = stylist.bio.toLowerCase()
+      const area = stylist.area.toLowerCase()
+      const searchQuery = this.state.searchQuery.toLowerCase()
+
+      return first_name.includes(searchQuery)
+        || last_name.includes(searchQuery)
+        || bio.includes(searchQuery)
+        || area.includes(searchQuery)
+    })
+
+
+
   render () {
     return(
       <div>
-        <Search/>
-        {this.props.stylists.map(stylist =>
+        <Search updateSearch={this.updateSearch}/>
+        {this.filterMuas().map(stylist =>
           <Stylist stylist={stylist}/>
         )}
       </div>
